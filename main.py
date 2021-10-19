a1c00412c14583ec4051c468ce0ee33c55bbf041
EndofSmallSanctuary <teenagefeverhotline@gmail.com>
2021-10-19 20:06:31 +0300

    m

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.globals import request
+from flask.templating import render_template
+app = Flask(__name__)
+
+
+def initiateapp8080():
+    app.run(debug=True,host="0.0.0.0",port=8081)
+
+
+@app.route('/',methods=['GET','POST'], defaults={'path': ''})
+@app.route('/<path:path>',methods=['GET','POST'])
+def request80(path):
+    return "Hello World 2"
+
+if __name__=="__main__":
+    initiateapp8080()
