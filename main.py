93fc2b0751a31b077862095a4887aaddede53f73
EndofSmallSanctuary <teenagefeverhotline@gmail.com>
2021-10-21 20:09:59 +0300

    m

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def initiateapp8080():
-    app.run(debug=True,host="0.0.0.0",port=8081)
+    app.run(debug=True,host="0.0.0.0",port=8080)
 
 
 @app.route('/',methods=['GET','POST'], defaults={'path': ''})
